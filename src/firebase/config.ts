b10acbf69219e0b23c0fe4ab5d6b010eaaa08013
Miller Quinn Laña <miller.lana@jmc.edu.ph>
2026-01-14 06:53:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3107621943-c7bb1",
+  "appId": "1:197659293167:web:35fbf40a5565353795b21a",
+  "apiKey": "AIzaSyBq-yCh0bk8jy0BfR2MkWBZ-w0h-QE-8QI",
+  "authDomain": "studio-3107621943-c7bb1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "197659293167"
+};
